fe2977cfd0796f7b6f2f20dd49b21b482ada162b
Katherine Tornwall <ktornwall@gmail.com>
2016-09-01 18:06:24 -0400

    Add non-page object example test

--- test/acceptance/summoners/index_test.exs
+++ test/acceptance/summoners/index_test.exs
@@ -3,6 +3,17 @@ defmodule ExConf.Acceptance.SummonersIndexTest do
 
   alias ExConf.{HomePage, SummonerIndexPage}
 
+  test "Searching for a summoner, without page objects" do
+    summoner = insert(:summoner, name: "katzenbar")
+
+    navigate_to "/"
+    fill_field({:class, "qa-summoner-search-input"}, summoner.name)
+    click({:class, "qa-summoner-search-submit"})
+
+    assert current_path == "/summoners"
+    assert find_element(:class, "qa-summoner-#{summoner.id}")
+  end
+
   test "Searching for a summoner" do
     summoner = insert(:summoner, name: "katzenbar")
 
